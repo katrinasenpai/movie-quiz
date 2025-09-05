82d45861fe185026a3cb25e42cabf1d62b0567c2
rz-dsgn <rz-dsgn@liber.local>
2025-09-05 14:38:10 +0300

    add docs as regular folder

---
+++ docs/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCcam04I594Zl-t83DGNH4eIcTBynJZweQ",
+    authDomain: "my-quiz-app-7e68f.firebaseapp.com",
+    projectId: "my-quiz-app-7e68f",
+    storageBucket: "my-quiz-app-7e68f.appspot.com",
+    messagingSenderId: "1037037283720",
+    appId: "1:1037037283720:web:1f5b05864587f3820e723d"
+};
